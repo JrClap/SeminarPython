e413c9af9de2f9ff3f02b01bdab4df246a9d7418
Uladzimir <blind72@mail.ru>
2023-01-23 19:27:23 +0100

    Fixed Task_3 in Seminar_6

--- Seminar_6/Task_3.py
+++ Seminar_6/Task_3.py
@@ -1,44 +1,50 @@
 #1. Напишите программу вычисления арифметического выражения заданного строкой. Используйте операции +,-,/,* приоритет операций стандартный.
 #* Добавьте скобки, приоритет операций меняется.
+#exp = -2 + ( 4 / 2 - 7 + 8 * 7 ) * 3
 
-#exp = "-2 + ( 4 / 2 - 7 + 8 * 7 ) * 3".split()
 actions = {
-"^": lambda x, y: str(float(x) ** float(y)),
-"*": lambda x, y: str(float(x) * float(y)),
-"/": lambda x, y: str(float(x) / float(y)),
-"+": lambda x, y: str(float(x) + float(y)),
-"-": lambda x, y: str(float(x) - float(y))
+    "^": lambda x, y: str(float(x) ** float(y)),
+    "*": lambda x, y: str(float(x) * float(y)),
+    "/": lambda x, y: str(float(x) / float(y)),
+    "+": lambda x, y: str(float(x) + float(y)),
+    "-": lambda x, y: str(float(x) - float(y))
 }
 
-def cut (lst):
-    new_list = []
+def cut (Ls):
+    lst = []
     temp = 0
-    while temp < len(lst):
-        if lst[temp] == '(':
-            end = lst.index(')')
-            new_list.append(lst[temp + 1 : end])
+    
+    while temp < len(Ls):
+        if Ls[temp] == '(':
+            end = Ls.index(')')
+            lst.append(Ls[temp + 1 : end])
             temp = end
         else:
-            new_list.append(lst[temp])
-            temp += 1
-    return new_list
+            lst.append(Ls[temp])
+        temp += 1
+    
+    return lst
 
-def calculat (f):
-    for i, k in enumerate(f):
-        if isinstance(k, list):
-            f[i] = calculat(k)
-    index_lst =[i for i, k in enumerate(f) if k in '*/']
+def calculat (new_lst):
+    for i, val in enumerate(new_lst):
+        if isinstance(val, list):
+            new_lst[i] = calculat(val)
+
+    index_lst =[i for i, val in enumerate(new_lst) if val in '*/']
+    
     while index_lst:
         index_op = index_lst[0]
-        a, b, c = f[index_op - 1 : index_op + 2]
-        f.insert(index_op - 1, actions[b](a, c))
-        del f [index_op : index_op + 3]
-        index_lst =[i for i, k in enumerate(f) if k in '*/']
-    while len(f) > 1:
-        a, b, c = f[:3]
-        del f[:3]
-        f.insert(0, actions[b](a, c))
-    return f[0]
+        a, b, c = new_lst[index_op - 1: index_op + 2]
+        new_lst.insert(index_op - 1, actions[b](a, c))
+        del new_lst [index_op: index_op + 3]
+        index_lst =[i for i, val in enumerate(new_lst) if val in '*/']
+    
+    while len(new_lst) > 1:
+        a, b, c = new_lst[:3]
+        del new_lst[:3]
+        new_lst.insert(0, actions[b](a, c))
+    
+    return new_lst[0]
 
 user_input = input("Input - ").split()
 print(calculat(cut(user_input)))
