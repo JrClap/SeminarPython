07f0e34d52f899fe862afa89afc29ee00a714dbe
Uladzimir <blind72@mail.ru>
2023-01-23 19:31:09 +0100

    Fixed Seminar_6

--- Seminar_6/HW_Seminar_6/Task2.py
+++
@@ -1,9 +0,0 @@
-# 2. Для чисел в пределах от 20 до N найти числа, кратные 20 или 21. Use comprehension.
-
-N = int(input('Введите количество элементов списка - '))
-
-def find_multiples(number):
-    lst = [i for i in range(20, N + 1) if i % 20 == 0 or i % 21 == 0]
-    print(lst)
-
-find_multiples(N)
